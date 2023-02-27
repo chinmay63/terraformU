3a276caac2a1c90ff6277bd0c7d5bbb2dfa91046
chinmay <linuxsaaga10@gmail.com>
2023-02-28 03:22:06 +0530

    commiting provider

--- createInstance.tf
+++ createInstance.tf
@@ -1,12 +1,3 @@
-
-provider "aws" {
-    version = "3.53.0"
-    access_key = "AKIASJE6YHJYGMWD7LDH"
-    secret_key = "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"
-    region     = "us-east-2"
-}
-
-
 resource "aws_instance" "MyFirstInstnace" {
   ami           = "ami-0a561b65214a47cac"
   instance_type = "t2.micro"
